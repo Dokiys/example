eda88328decda47aacd126e1e483ffdc32d9f786
Dokiy <zhangzongqi@fancydigital.com.cn>
2021-11-04 11:40:28 +0800

    fix

--- mydoc/doc.go
+++ mydoc/doc.go
@@ -1,3 +1,4 @@
+//  git clone git@github.com:Dokiys/go_test.git
 //	In terminal run:
 //		godoc -http=:6060
 //	and check in http://localhost:8080/pkg/go_test/mydoc/
@@ -5,6 +6,6 @@
 /*
 	Package mydoc shows how to use go doc
 
-	This project is noted from https://blog.cyeam.com/golang/2018/09/03/godoc
+	This project notes from https://blog.cyeam.com/golang/2018/09/03/godoc
  */
 package mydoc
