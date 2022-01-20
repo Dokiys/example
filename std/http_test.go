8b621e168e7721cec4274e0591d10a5308cfe007
Dokiy <zhangzongqi@fancydigital.com.cn>
2022-01-20 19:53:28 +0800

    add http get post

--- std/http_test.go
+++ std/http_test.go
@@ -1,7 +1,12 @@
 package std
 
 import (
+	"bytes"
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"net/http"
+	"strconv"
 	"testing"
 )
 
@@ -19,4 +24,50 @@ func TestHttpServer(t *testing.T) {
 		panic(err)
 	}
 	select {}
+}
+
+type Resp struct {}
+type Req struct {}
+// TestHttpSendGet 测试http发送Get请求
+func TestHttpSendGet(t *testing.T) {
+	req, err := http.NewRequest("GET", "http://www.4399.com", nil)
+	assert.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+	// 设置参数
+	params := req.URL.Query()
+	params.Add("id", strconv.FormatInt(1324, 10))
+	req.URL.RawQuery = params.Encode()
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	assert.NoError(t, err)
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	assert.NoError(t, err)
+
+	data := Resp{}
+	assert.NoError(t, json.Unmarshal(body, &data))
+}
+
+// TestHttpSendPost 测试http发送Post请求
+func TestHttpSendPost(t *testing.T) {
+	param := Req{}
+	bodyData, err := json.Marshal(param)
+	assert.NoError(t, err)
+
+	req, err := http.NewRequest( "POST", "http://www.4399.com", bytes.NewReader(bodyData))
+	assert.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	assert.NoError(t, err)
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	assert.NoError(t, err)
+
+	data := Resp{}
+	assert.NoError(t, json.Unmarshal(body, &data))
 }
