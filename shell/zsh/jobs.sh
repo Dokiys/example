c3036b6b412b59ff219dd76d07c3f4ac272b7f72
Dokiy <dokiychang@gmail.com>
2022-12-08 11:26:57 +0800

    add zsh iterm

--- shell/zsh/jobs.sh
+++ shell/zsh/jobs.sh
@@ -5,6 +5,12 @@
 protofmt_content='
 # protofmt buf format ./*.proto file
 function protofmt() {
+  buf --version > /dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    echo 1>&2 "protofmt: command not found: buf"
+    echo 1>&2 "  (run: go install github.com/bufbuild/buf/cmd/buf@latest to install)";
+    return;
+  fi
   for filename in $(find . -name "*.proto"); do
     buf format $filename -o $filename;
   done
@@ -16,4 +22,12 @@ gnum_content='
 # gnum generate every N..M num on each line
 function gnum() {
   echo {$1..$2} | tr " " "\n" | pbcopy
+}'
+
+# iterm
+# shellcheck disable=SC2034
+iterm_content='
+# iterm launch new iterm2 tab at specail path
+function iterm() {
+  open -a iterm $1
 }'
